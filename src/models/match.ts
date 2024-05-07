e2431fc90517e5f6b126824e89818b6c39b7a7cc
Patryk Ploszaj <patryk.ploszaj07@gmail.com>
2024-05-07 11:05:23 -0400

    Add getters and setters for Match class

--- src/models/match.ts
+++ src/models/match.ts
@@ -1,10 +1,31 @@
 export class Match {
-    private _homeScore: number = 0;
-    private _awayScore: number = 0;
-    constructor(
-        public homeTeam: string,
-        public awayTeam: string,
-        public startTime: Date = new Date()
-    ){}
+  private _homeScore: number = 0;
+  private _awayScore: number = 0;
+  constructor(
+    public homeTeam: string,
+    public awayTeam: string,
+    public startTime: Date = new Date()
+  ) {}
 
+  get homeScore(): number {
+    return this._homeScore;
+  }
+
+  set homeScore(value: number) {
+    if (value < 0) {
+      throw new Error("homeScore cannot be negative");
+    }
+    this._homeScore = value;
+  }
+
+  get awayScore(): number {
+    return this._awayScore;
+  }
+
+  set awayScore(value: number) {
+    if (value < 0) {
+      throw new Error("awayScore cannot be negative");
+    }
+    this._awayScore = value;
+  }
 }
